5fe4ea2aa1a78d2243072862be02489bec4ef2e2
Frank H.J. Du <Frankdew1995@users.noreply.github.com>
2018-02-07 04:12:07 +0100

    Update pipelines.py

--- Windeln_Bot/pipelines.py
+++ Windeln_Bot/pipelines.py
@@ -4,7 +4,7 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-import pymongo
+# import pymongo
 import logging
 
 class WindelnBotPipeline(object):
