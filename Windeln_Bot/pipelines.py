93f94b5b3eb9e943afc44d688684a8c47cba4ee9
Frank H.J. Du <Frankdew1995@users.noreply.github.com>
2018-02-07 04:14:05 +0100

    Update pipelines.py

--- Windeln_Bot/pipelines.py
+++ Windeln_Bot/pipelines.py
@@ -5,40 +5,40 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 # import pymongo
-import logging
+# import logging
 
 class WindelnBotPipeline(object):
     def process_item(self, item, spider):
         return item
 
-class MongoPipeline(object):
+# class MongoPipeline(object):
 
-    collection_name = 'Windeln'
+#     collection_name = 'Windeln'
 
-    def __init__(self, mongo_uri, mongo_db):
-        self.mongo_uri = mongo_uri
-        self.mongo_db = mongo_db
+#     def __init__(self, mongo_uri, mongo_db):
+#         self.mongo_uri = mongo_uri
+#         self.mongo_db = mongo_db
 
-    @classmethod
-    def from_crawler(cls, crawler):
-        ## pull in information from settings.py
-        return cls(
-            mongo_uri=crawler.settings.get('MONGO_URI'),
-            mongo_db=crawler.settings.get('MONGO_DATABASE')
-        )
+#     @classmethod
+#     def from_crawler(cls, crawler):
+#         ## pull in information from settings.py
+#         return cls(
+#             mongo_uri=crawler.settings.get('MONGO_URI'),
+#             mongo_db=crawler.settings.get('MONGO_DATABASE')
+#         )
 
-    def open_spider(self, spider):
-        ## initializing spider
-        ## opening db connection
-        self.client = pymongo.MongoClient(self.mongo_uri)
-        self.db = self.client[self.mongo_db]
+#     def open_spider(self, spider):
+#         ## initializing spider
+#         ## opening db connection
+#         self.client = pymongo.MongoClient(self.mongo_uri)
+#         self.db = self.client[self.mongo_db]
 
-    def close_spider(self, spider):
-        ## clean up when spider is closed
-        self.client.close()
+#     def close_spider(self, spider):
+#         ## clean up when spider is closed
+#         self.client.close()
 
-    def process_item(self, item, Balea):
-        ## how to handle each post
-        self.db[self.collection_name].insert(dict(item))
-        logging.debug("Post added to MongoDB")
-        return item
+#     def process_item(self, item, Balea):
+#         ## how to handle each post
+#         self.db[self.collection_name].insert(dict(item))
+#         logging.debug("Post added to MongoDB")
+#         return item
