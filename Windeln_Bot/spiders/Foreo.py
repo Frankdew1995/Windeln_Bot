54a01ca01fb024a462e33148a63e54c84d709a56
Frank H.J. Du <Frankdew1995@users.noreply.github.com>
2018-02-07 04:06:46 +0100

    Add files via upload

---
+++ Windeln_Bot/spiders/Foreo.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from Windeln_Bot.items import WindelnBotItem
+
+
+class BabybjoernSpider(scrapy.Spider):
+    name = 'Foreo'
+    allowed_domains = ['windeln.de']
+    start_urls = ['https://www.windeln.com.cn/foreo/']
+
+    def parse(self, response):
+        item = WindelnBotItem()
+        products = response.css("div.productlink-inner")
+        for product in products:
+            item['eans'] = product.css("a::attr(eans)").extract_first()
+            item['name'] = product.css("a::attr(title)").extract_first()
+            item['img'] = product.css("a > div.wrapper > img::attr(data-src)").extract_first()
+            yield item
+
